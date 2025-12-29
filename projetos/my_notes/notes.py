a4be74bdb20f7a839b53830e2d1ae3f676449d48
Klaudio Silva <da.silva.klaudio@outook.com>
2025-12-29 19:42:54 -0300

    adicionando menu editar

--- projetos/my_notes/notes.py
+++ projetos/my_notes/notes.py
@@ -16,24 +16,25 @@ area_txt.pack(expand=1, fill='both')
 #----------------------------------
 def opn_file():
     caminho = filedialog.askopenfilename(defaultextension='.txt',
-                                         filetypes=[('Arquivos de texto', '*.txt'),
-                                         ('Todos os arquivos', '*.*')])
+                                         filetypes=[('Arquivos de texto', '*.txt'), ('Todos os arquivos', '*.*')])
     
     if caminho:
         with open(caminho, 'r', encoding='utf-8') as f:
             conteudo = f.read()
+        area_txt.delete(1.0, tk.END)
+        area_txt.insert(tk.END, conteudo)
 
 def sav_file():
-    caminho = filedialog.asksaveasfile(defaultextension='.txt',
-                                       filetypes=[('Arquivos de texto', '*.txt'),
-                                        ('Todos os arquivos', '*.*')])
+    caminho = filedialog.asksaveasfilename(defaultextension='.txt',
+                                       filetypes=[('Arquivos de texto', '*.txt'), ('Todos os arquivos', '*.*')])
     
     if caminho:
-        with open(caminho, 'm', encoding='utf-8') as f:
+        with open(caminho, 'w', encoding='utf-8') as f:
             f.write(area_txt.get(1.0, tk.END))
         messagebox.showinfo('Salvar', 'Arquivo salvo corretamente')
 
-'''
+
+
 #-------------------------------------
 #menu superior
 #-------------------------------------
@@ -41,14 +42,28 @@ bar_menu = tk.Menu(root)
 
 #menu Arquivo
 mn_file = tk.Menu(bar_menu, tearoff=0)
-mn_file.add_command(label='Abrir', command=opn_file)
+mn_file.add_command(label='Novo')
+mn_file.add_command(label='Abrir...', command=opn_file)
 mn_file.add_command(label='Salvar', command=sav_file)
+mn_file.add_command(label='Salvar como...')
+mn_file.add_separator()
+mn_file.add_command(label='Configurar página...')
+mn_file.add_command(label='Imprimir...')
 mn_file.add_separator()
 mn_file.add_command(label='Fechar', command=root.quit)
 
-mn_file.add_cascade(label='Arquivo', menu=mn_file)
+bar_menu.add_cascade(label='Arquivo', menu=mn_file)
 
-root.config(menu=mn_file)
-'''
+root.config(menu=bar_menu)
+
+#menu Editar
+mn_edit = tk.Menu(bar_menu, tearoff=0)
+mn_edit.add_command(label='Copiar')
+mn_edit.add_command(label='Copiar')
+mn_edit.add_command(label='Copiar')
+mn_edit.add_command(label='Copiar')
+
+bar_menu.add_cascade(label='Editar', menu=mn_edit)
+
 
 root.mainloop()
